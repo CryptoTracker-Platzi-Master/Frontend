0ec4ac7ca35f9bb1ca951b1ce2ee1faa55be90c6
Leonardo Rincón <jleobotache@gmail.com>
2021-06-14 16:33:07 -0500

    I create the Footer component

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 
 import { Header } from "../../components/Header";
-import {} from "../../assets/css/App.css";
+import { Footer } from "../../components/Footer";
+import "../../assets/css/App.css";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 };
