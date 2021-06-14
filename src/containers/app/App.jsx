3b7f635c55fd4ca44363713f34ab223bcf310fca
Leonardo Rincón <jleobotache@gmail.com>
2021-06-14 17:01:39 -0500

    I create the Home pages and add the styles

--- src/containers/app/App.jsx
+++ src/containers/app/App.jsx
@@ -1,14 +1,13 @@
 import React, { Fragment } from "react";
 
-import { Header } from "../../components/Header";
-import { Footer } from "../../components/Footer";
+import { Home } from "../../pages/Home";
+
 import "../../assets/css/App.css";
 
 export const App = () => {
   return (
     <>
-      <Header />
-      <Footer />
+      <Home />
     </>
   );
 };
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import "./Home.scss";
+import currencyChange from "../../assets/img/currency-exchange-mobile.png";
+
+import { Header } from "../../components/Header";
+import { Footer } from "../../components/Footer";
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <main className="home">
+        <h1 className="home--title">CryptoTracker</h1>
+        <p className="home--description">
+          CryptoTracker is a web application where you can track the top 10
+          cryptocurrencies. Being able to keep your portfolio and know how
+          healthy it is, also know if it is a good time to buy or sell!
+        </p>
+        <figure className="home__container-img">
+          <img
+            className="home__container-img--img"
+            src={currencyChange}
+            alt="imagen de Currency Change"
+          />
+        </figure>
+      </main>
+      <Footer />
+    </>
+  );
+};
