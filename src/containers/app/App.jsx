c9823b6b1db3dc566347311e408e52f6487d298d
Leonardo Rincón <jleobotache@gmail.com>
2021-06-14 15:57:36 -0500

    I create the files of the variables and mixins of sass, I also organize the structure of the folders and create the base of the component Header.

---
+++ src/containers/app/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+import { Header } from "../../components/Header";
+import {} from "../../assets/css/App.css";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
