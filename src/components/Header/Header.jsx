71ea44379d545b254f556b006b108402d5ef05d7
Leonardo Rincón <jleobotache@gmail.com>
2021-06-14 16:22:24 -0500

    I create the Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+import logoTipo from "../../assets/img/logoTipo.png";
+import "./Header.scss";
+
 export const Header = () => {
-  return <h1>header</h1>;
+  return (
+    <header className="header">
+      <figure className="header__container-img">
+        <img className="header__container-img--img" src={logoTipo} alt="logo" />
+      </figure>
+      <nav className="header__nav">
+        <button className="header__nav--login" type="button">
+          Login
+        </button>
+        <button className="header__nav--sign-up" type="button">
+          Sign Up
+        </button>
+      </nav>
+    </header>
+  );
 };
