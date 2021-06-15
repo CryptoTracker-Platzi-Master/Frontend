f020a1c8e094939d0fce50dcd53a783119a901bc
Leonardo Rincón <jleobotache@gmail.com>
2021-06-15 16:38:07 -0500

    create the base structure of the dashboard page

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -11,28 +11,34 @@ export const Dashboard = () => {
         <button className="dashboard__wrap-card--card--btn-plus" type="button">
           <i class="fas fa-plus-circle"></i>
         </button>
-        <div className="dashboard__wrap-card--card--title">
-          <figure>
-            <img src="" alt="" />
+        <div className="dashboard__wrap-card--card__title">
+          <figure className="dashboard__wrap-card--card__title--container-img">
+            <img
+              className="dashboard__wrap-card--card__title--container-img--img"
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+              alt=""
+            />
           </figure>
           <h3>
             Bitcoin - <span>BTC</span>
           </h3>
         </div>
-        <div className="dashboard__wrap-card--card--description">
+        <div className="dashboard__wrap-card--card__description">
           <h4>
-            Price: <span>$</span>
+            Price: <span className="currency">$</span>
             <span>36000</span>
           </h4>
           <h4>
-            Volume 24h:<span>$</span>
+            Volume 24h:<span className="currency">$</span>
             <span>6000</span>
           </h4>
           <h4>
-            Market Cap:<span>$</span>
+            Market Cap:<span className="currency">$</span>
             <span>6000</span>
           </h4>
-          <h4>24h %: 5</h4>
+          <h4>
+            24h %: <span className="percentage-24h">5</span>
+          </h4>
         </div>
       </div>
     );
